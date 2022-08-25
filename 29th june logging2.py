1e44f1c363171a0f7ec29cddeab9d5d4f76747e2
yuvraj <67325433+Yuvrajanalyst@users.noreply.github.com>
2022-08-25 17:09:42 +0530

    Add files via upload

--- 29th june logging2.py
+++ 29th june logging2.py
@@ -1,7 +1,15 @@
-import logging as logs
+import logging
 
-formt = ('%(asctime)s | %(message)s')
-logs.basicConfig(filename='test2.log',level=logs.INFO, filemode='w',format=formt)
-def devide(a,b):
-    return a/b
-print(devide(3,0))
+formt = ('%(asctime)s | %(name)s | %(levelname)s | %(message)s ')
+logging.basicConfig(filename='test2.log', level=logging.DEBUG, filemode='w',format=formt)
+
+def divi(a,b):
+    logging.info('The number user enterted by %s and %s', a,b)
+    try:
+        logging.info('we are into function')
+        div = a/b
+        logging.info('This is the ans of %s', div)
+        logging.info('we are completed a division operation.')
+    except Exception as e:
+        logging.exception(e)
+divi(3,1)
